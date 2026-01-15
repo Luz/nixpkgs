29987d01ef2211943e125efd1b30436efae316ed
Luz <luz666@daum.net>
2026-01-15 23:04:50 +0100

    librepcb: 1.3.0 -> 2.0.0

--- pkgs/applications/science/electronics/librepcb/default.nix
+++ pkgs/applications/science/electronics/librepcb/default.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "librepcb";
-  version = "1.3.0";
+  version = "2.0.0-rc1";
 
   src = fetchFromGitHub {
     owner = "librepcb";
     repo = "librepcb";
     rev = version;
-    hash = "sha256-J4y0ikZNuOguN9msmEQzgcY0/REnOEOoDkY/ga+Cfd8=";
+    hash = "sha256-cdDadKVMnEofBzb4hQMyGkrCAYuYlijKm7hA0LRXMMs=";
     fetchSubmodules = true;
   };
 
@@ -35,26 +35,45 @@ stdenv.mkDerivation rec {
     wrapQtAppsHook
     opencascade-occt
     libGLU
-    rustPlatform.cargoSetupHook
     cargo
     rustc
   ];
   buildInputs = [ qtbase ];
 
-  cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit src cargoRoot;
+  cargoDeps1 = rustPlatform.fetchCargoVendor {
+    inherit src;
+    cargoRoot = "libs/librepcb/rust-core";
     hash = "sha256-1td3WjxbDq2lX7c0trpYRhO82ChNAG/ZABBRsekYtq4=";
   };
 
-  cargoRoot = "libs/librepcb/rust-core";
+  cargoDeps2 = rustPlatform.fetchCargoVendor {
+    inherit src;
+    cargoRoot = "libs/slint";
+    hash = "sha256-DYcKoaOXYFvAi5VyWdhli73s7qrypeXmzGJNhVzcWtY=";
+  };
+
 
   postPatch = ''
-    substituteInPlace libs/muparser/CMakeLists.txt \
-      --replace-fail "cmake_minimum_required (VERSION 3.1.0)" "cmake_minimum_required(VERSION 3.10)"
-    substituteInPlace libs/type_safe{/,/external/debug_assert/}CMakeLists.txt \
-      --replace-fail "cmake_minimum_required(VERSION 3.1)" "cmake_minimum_required(VERSION 3.10)"
-    substituteInPlace libs/googletest{/,/googlemock/,/googletest/}CMakeLists.txt \
-      --replace-fail "cmake_minimum_required(VERSION 3.2)" "cmake_minimum_required(VERSION 3.10)"
+    substituteInPlace libs/librepcb/core/build_env.h.in \
+      --replace-fail "@GIT_COMMIT_TIMESTAMP@" "0"
+
+    # Set up cargo config for the first Rust library
+    mkdir -p libs/librepcb/rust-core/.cargo
+    cat > libs/librepcb/rust-core/.cargo/config.toml <<EOF
+    [source.crates-io]
+    replace-with = "vendored-sources"
+    [source.vendored-sources]
+    directory = "${cargoDeps1}"
+    EOF
+
+    # Set up cargo config for the second Rust library
+    mkdir -p libs/slint/.cargo
+    cat > libs/slint/.cargo/config.toml <<EOF
+    [source.crates-io]
+    replace-with = "vendored-sources"
+    [source.vendored-sources]
+    directory = "${cargoDeps2}"
+    EOF
   '';
 
   meta = {
